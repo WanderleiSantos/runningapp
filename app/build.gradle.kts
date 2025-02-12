b1837239891099771d4ba57121101212306d160a
Wanderlei Santos <wanderleidias.santos@hotmail.com>
2025-02-12 13:16:16 -0300

    Initial Commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,96 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    id("kotlin-kapt")
+}
+
+android {
+    namespace = "com.manodev.runningapp"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.manodev.runningapp"
+        minSdk = 28
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    // Architectural Components
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+
+    // Room
+    implementation(libs.androidx.room.runtime)
+    kapt(libs.androidx.room.compiler)
+
+    // Kotlin Extensions and Coroutines support for Room
+    implementation(libs.androidx.room.ktx)
+
+    // Coroutines
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android)
+
+    // Coroutine Lifecycle Scopes
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+
+    // Navigation Components
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+
+    // Glide
+    implementation(libs.glide)
+    kapt("com.github.bumptech.glide:compiler:4.11.0")
+
+    // Google Maps Location Services
+    implementation(libs.play.services.location)
+    implementation(libs.play.services.maps)
+
+    // Dagger Core
+    implementation(libs.dagger)
+    kapt(libs.dagger.compiler)
+
+    // Dagger Android
+    api(libs.dagger.android)
+    api(libs.dagger.android.support)
+    kapt(libs.dagger.android.processor)
+
+    // Easy Permissions
+    implementation(libs.easypermissions)
+
+    // Timber
+    implementation(libs.timber)
+
+    // MPAndroidChart
+
+}
